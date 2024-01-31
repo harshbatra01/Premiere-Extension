c4a3e9039b4cb702c5509067e22150204c00f8d7
harsh batra <harshbatra@harshs-MacBook-Air.local>
2024-01-31 17:29:22 +0530

    some code commented in addfiles

--- jsx/aftereffects.jsx
+++ jsx/aftereffects.jsx
@@ -2,48 +2,63 @@
 //   var clipName = app.project.sequence[0].videoTracks[0].clips[0].name;
 // }
 
-function addFiles(){ 
-  alert("hi" );
-  var elements = [
-            {
-              "asset": "computer-display",
-              "timestamp": 4
-            },
-            {
-              "asset": "right arrow",
-              "timestamp": 18
-            },
-            {
-              "asset": "clock-time",
-              "timestamp": 24
-            },
-            {
-              "asset": "videocam",
-              "timestamp": 29
-            }
-          ];
-      var elem = {
-        index: 0,
-        message: {
-          role: 'assistant',
-          content: [{"asset": "document", "timestamp": 0}]
-        },
-        logprobs: null,
-        finish_reason: 'stop'
-      }
+function addFiles() {
+  alert("hdi");
 
-      var elements = elem.message.content;
-      for (var i = 0; i < elements.length; i++) {
-        var element = elements[i];
-        var time = element.timestamp;
-        var name = element.asset;
-        var filePath = "/Users/harshbatra/Downloads/Animated Icons/" + name + ".gif"; 
-        var importCompleted = app.project.importFiles([filePath], false);
-    
-        if (importCompleted) {
-            var importedItem = app.project.rootItem.children[(app.project.rootItem.children.length - 1)];
-            var sequence = app.project.activeSequence;
-            sequence.videoTracks[1].insertClip(importedItem, time);
-        }
-      }
-}
+//   var elements = [
+//     { asset: "computer-display", timestamp: 4 },
+//     { asset: "right arrow", timestamp: 18 },
+//     { asset: "clock-time", timestamp: 24 },
+//     { asset: "videocam", timestamp: 29 }
+//   ];
+
+//   // var elem = {
+//   //   index: 0,
+//   //   message: {
+//   //     role: "assistant",
+//   //     content: [{ asset: "document", timestamp: 0 }]
+//   //   },
+//   //   logprobs: null,
+//   //   finish_reason: "stop"
+//   // };
+ 
+//   // var elements = elem.message.content;
+
+//   if (!elements || elements.length === 0) {
+//     alert("Error: Assistant elements not created.");
+//     return;
+//   }
+//   alert("hi2")
+
+//   for (var i = 0; i < elements.length; i++) {
+//     var element = elements[i];
+//     var time = element.timestamp;
+//     var name = element.asset;
+//     var filePath = "/Users/harshbatra/Downloads/Animated Icons/" + name + ".gif";
+ 
+//     var importCompleted = app.project.importFiles([filePath], false);
+//  alert('hi2')
+//     if (importCompleted) {
+//       var importedItem = app.project.rootItem.children[app.project.rootItem.children.length - 1];
+//       alert("import completed");
+//       // Check for imported item
+//       if (importedItem) {
+//         var sequence = app.project.activeSequence;
+
+//         // Check for active sequence
+//         if (sequence) {
+//           try {
+//             sequence.videoTracks[1].insertClip(importedItem, time);
+//           } catch (error) {
+//             alert("Error inserting clip:", error);
+//           }
+//         } else {
+//           alert("Active sequence not found.");
+//         }
+//       } else {
+//         alert("Imported item not found.");
+//       }
+//     }
+//   }
+ }
+ 
