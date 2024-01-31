df825549d9453d2dd973b3bdf777d288f4b1dc9b
harsh batra <harshbatra@harshs-MacBook-Air.local>
2024-01-31 18:49:59 +0530

    duplicated button for test

--- jsx/aftereffects.jsx
+++ jsx/aftereffects.jsx
@@ -3,62 +3,62 @@
 // }
 
 function addFiles() {
+ 
   alert("hdi");
+  var elements = [
+    { asset: "computer-display", timestamp: 4 },
+    { asset: "right arrow", timestamp: 18 },
+    { asset: "clock-time", timestamp: 24 },
+    { asset: "videocam", timestamp: 29 }
+  ];
 
-//   var elements = [
-//     { asset: "computer-display", timestamp: 4 },
-//     { asset: "right arrow", timestamp: 18 },
-//     { asset: "clock-time", timestamp: 24 },
-//     { asset: "videocam", timestamp: 29 }
-//   ];
-
-//   // var elem = {
-//   //   index: 0,
-//   //   message: {
-//   //     role: "assistant",
-//   //     content: [{ asset: "document", timestamp: 0 }]
-//   //   },
-//   //   logprobs: null,
-//   //   finish_reason: "stop"
-//   // };
+  // var elem = {
+  //   index: 0,
+  //   message: {
+  //     role: "assistant",
+  //     content: [{ asset: "document", timestamp: 0 }]
+  //   },
+  //   logprobs: null,
+  //   finish_reason: "stop"
+  // };
  
-//   // var elements = elem.message.content;
+  // var elements = elem.message.content;
 
-//   if (!elements || elements.length === 0) {
-//     alert("Error: Assistant elements not created.");
-//     return;
-//   }
-//   alert("hi2")
+  if (!elements || elements.length === 0) {
+    alert("Error: Assistant elements not created.");
+    return;
+  }
+  alert("hi2")
 
-//   for (var i = 0; i < elements.length; i++) {
-//     var element = elements[i];
-//     var time = element.timestamp;
-//     var name = element.asset;
-//     var filePath = "/Users/harshbatra/Downloads/Animated Icons/" + name + ".gif";
+  for (var i = 0; i < elements.length; i++) {
+    var element = elements[i];
+    var time = element.timestamp;
+    var name = element.asset;
+    var filePath = "/Users/harshbatra/Downloads/Animated Icons/" + name + ".gif";
  
-//     var importCompleted = app.project.importFiles([filePath], false);
-//  alert('hi2')
-//     if (importCompleted) {
-//       var importedItem = app.project.rootItem.children[app.project.rootItem.children.length - 1];
-//       alert("import completed");
-//       // Check for imported item
-//       if (importedItem) {
-//         var sequence = app.project.activeSequence;
+    var importCompleted = app.project.importFiles([filePath], false);
+ alert('hi2')
+    if (importCompleted) {
+      var importedItem = app.project.rootItem.children[app.project.rootItem.children.length - 1];
+      alert("import completed");
+      // Check for imported item
+      if (importedItem) {
+        var sequence = app.project.activeSequence;
 
-//         // Check for active sequence
-//         if (sequence) {
-//           try {
-//             sequence.videoTracks[1].insertClip(importedItem, time);
-//           } catch (error) {
-//             alert("Error inserting clip:", error);
-//           }
-//         } else {
-//           alert("Active sequence not found.");
-//         }
-//       } else {
-//         alert("Imported item not found.");
-//       }
-//     }
-//   }
+        // Check for active sequence
+        if (sequence) {
+          try {
+            sequence.videoTracks[1].insertClip(importedItem, time);
+          } catch (error) {
+            alert("Error inserting clip:", error);
+          }
+        } else {
+          alert("Active sequence not found.");
+        }
+      } else {
+        alert("Imported item not found.");
+      }
+    }
+  }
  }
  
